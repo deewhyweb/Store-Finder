9f57d3b51f8de3b7d7253b72783c57816df301c0
Philip Hayes <phayes@Philips-MacBook-Pro-3.local>
2014-02-18 17:07:59 +0000

    test fix

--- client/default/js/app.js
+++ client/default/js/app.js
@@ -23,14 +23,10 @@
       //loadingMask.show();
       console.log('launching app');
       
-      if (Ext.is.Android) {
-        Ext.Anim.override({
-          disableAnimations: true
-        });
-      }
+   
       
       this.views.mainView = new this.views.MainView({
-        
+
       });
     }
   });
